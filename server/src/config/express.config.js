177158c93422f38b6f4477497ec5d9f5bd8a5bdf
this-is-neha <nehushah12@gmail.com>
2025-04-22 09:10:13 +0545

    Resolev

--- server/src/config/express.config.js
+++ server/src/config/express.config.js
@@ -1,88 +1,3 @@
-// const express = require("express");
-// const mongoose=require("mongoose");
-
-// require("./db.config")
-// const helmet = require("helmet")
-// const cors = require("cors")
-
-// const Joi = require('joi')
-
-// const app = express();
-// app.use(helmet())
-// app.use(cors())
-// const mainRouter = require("./routing.config")
-
-// const router = express.Router()
-// app.use(express.json())
-// app.use(express.urlencoded({
-//     extended: true
-// }))
-// app.use('/assets', express.static('./public/'))
-// router.get('/health', (req, res, next) => {
-//     res.json({
-//         result: "Hello there",
-//         message: "Successs OK",
-//         meta: null
-
-//     })
-// })
-// app.use(router)
-// app.use(mainRouter)
-
-// app.use((req, res, next) => {
-//     next({
-//         code: 404,
-//         message: "Resouce not found",
-
-//     })
-
-
-// });
-// app.use((error, req, res, next) => {
-// console.log(error instanceof mongoose.MongooseError)
-
-
-
-//     let statusCode = error.code || 500;
-//     let data = error.data || null;
-//     let msg = error.message || "Internal server error"
-
-
-//     if (error instanceof Joi.ValidationError) {
-//         statusCode = 422,
-//             msg = "Validation Failed",
-//             data = {};
-//         const errorDetail = error.details
-//         if (Array.isArray(errorDetail)) {
-//             errorDetail.map((errorObj) => {
-//                 data[errorObj.context.label] = errorObj.message
-//             })
-//         }
-//     }
-
-// if (+statusCode===11000){
-//     statusCode=400
-//     data={};
-//    const fields =Object.keys (error.keyPattern)
-//    fields.map((fieldname)=>{
-//     data[fieldname]=fieldname+"should be unique"
-//    })
-//    msg="Validation Failed"
-// }
-//     res.status(statusCode).json({
-//         result: data,
-//         message: msg,
-//         meta: null
-//     })
-
-// })
-
-
-
-
-
-// module.exports = app
-
 const express = require("express");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
@@ -95,7 +10,7 @@ const mainRouter = require("./routing.config");
 
 const app = express();
 
-// 🔐 Security & Middleware
+
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
@@ -118,7 +33,7 @@ app.get("/health", (req, res) => {
   });
 });
 
-// 📦 API Routes
+console.log("This si Neha");
 app.use(mainRouter);
 
 // ❌ 404 Handler
@@ -166,8 +81,4 @@ app.use((error, req, res, next) => {
   });
 });
 
-<<<<<<< HEAD
 module.exports = app;
-=======
-module.exports = app;
->>>>>>> db2d3d831f6a6008be275313f71597a58ae1d147
