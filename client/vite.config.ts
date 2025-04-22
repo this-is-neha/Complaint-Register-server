f391d767591993bb189757a1d28cb57cfdf65f2c
this-is-neha <nehushah12@gmail.com>
2025-04-22 14:10:52 +0545

    neaaajjjh

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// export default defineConfig({
+//   plugins: [react()],
+
+//   build: {
+//     rollupOptions: {
+//       external: ['framer-motion']
+//     }
+
+// }})
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-
+  base: "/", // 👈 SUPER important for correct routing on Netlify
   build: {
     rollupOptions: {
       external: ['framer-motion']
     }
-
-}})
+  }
+})
