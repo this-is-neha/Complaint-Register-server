52e898fd8fe6e667021107ad065b166fba29714b
this-is-neha <137779038+this-is-neha@users.noreply.github.com>
2025-04-22 08:26:38 +0545

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const http=require("http")
+const http = require("http");
+const app = require("./src/config/express.config");
 
+const port = process.env.PORT || 9006;  // Use the port from the environment variable or fallback to 9006
+const server = http.createServer(app);
 
-const app=require("./src/config/express.config")
-
-const server=http.createServer(app)
-server.listen(9006,err)=>{
-    if(!err){
-        console.log("Server is running successfully in 9006");
-        console.log("Press CTRL C to disconnect")
+server.listen(port, (err) => {
+    if (!err) {
+        console.log(`Server is running successfully on port ${port}`);
+        console.log("Press CTRL C to disconnect");
     }
-})
+});
