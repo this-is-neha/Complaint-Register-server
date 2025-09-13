69d713a7aaeef6009f6f8d47aaf4d89f5d916820
this-is-neha <nehushah12@gmail.com>
2025-09-13 13:46:41 +0545

    ADD

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -21,16 +21,14 @@
 // })
 
 
-
-
 require("dotenv").config();
 const mongoose = require("mongoose");
 
 const { MONGO_DB_URL, MONGO_DB_NAME } = process.env;
 
 mongoose.connect(MONGO_DB_URL, { dbName: MONGO_DB_NAME })
-    .then(() => console.log("MongoDB connected successfully..."))
-    .catch((err) => {
-        console.error("Error connecting to MongoDB:", err.message);
-        process.exit(1);
-    });
+  .then(() => console.log("MongoDB connected successfully..."))
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+    process.exit(1);
+  });
