fe1a1d58e20618b1adb6e065b81cb043fe53927b
this-is-neha <nehushah12@gmail.com>
2025-09-13 13:40:25 +0545

    nsh

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,25 +1,36 @@
+// require("dotenv").config();
+
+// const mongoose=require("mongoose")
+
+// console
+
+// const { MONGO_DB_URL, MONGO_DB_NAME } = process.env;
+
+// mongoose.connect(MONGO_DB_URL,{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     dbName:MONGO_DB_NAME
+   
+// }).then(()=>{
+//     console.log("Mongo db connnected successfully...")
+// })
+// .catch((err)=>{
+// console.log("Error while connecting Mongodb")
+// console.log("Error:",err    )
+// process.exit(1)
+// })
+
+
+
+
 require("dotenv").config();
-
-const mongoose=require("mongoose")
-
-console.log("Mongo URL:",process.env.MONGO_DB_URL)
+const mongoose = require("mongoose");
 
 const { MONGO_DB_URL, MONGO_DB_NAME } = process.env;
 
-mongoose.connect(MONGO_DB_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName:MONGO_DB_NAME
-   
-}).then(()=>{
-    console.log("Mongo db connnected successfully...")
-})
-.catch((err)=>{
-console.log("Error while connecting Mongodb")
-console.log("Error:",err    )
-process.exit(1)
-})
-
-
-
-
+mongoose.connect(MONGO_DB_URL, { dbName: MONGO_DB_NAME })
+    .then(() => console.log("MongoDB connected successfully..."))
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err.message);
+        process.exit(1);
+    });
